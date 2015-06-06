1b2a22c0b35f7985f92831397f64573ed49d8221
kudkudak <staszek.jastrzebski@gmail.com>
2015-06-06 14:33:27 +0200

    Clear notebooks

--- scripts/connect-cog.sh
+++ scripts/connect-cog.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+sudo umount -l cog
+sudo umount -l cog_data
+
 sshfs sjastrzebski@ailab.ii.uj.edu.pl:/home/sjastrzebski/al_ecml cog
 sshfs sjastrzebski@ailab.ii.uj.edu.pl:/home/sjastrzebski/al_ecml_big cog_data
 
--- scripts/connect-hub.sh
+++ scripts/connect-hub.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+sudo umount -l hub
+sudo umount -l hub_data
+
 sshfs staszek.jastrzebski@104.155.96.215:/home/staszek.jastrzebski/al_ecml hub
 sshfs staszek.jastrzebski@104.155.96.215:/home/staszek.jastrzebski/al_ecml-big hub_data
 
