4dfe6cd6a8521b052a000f8f79480be7fe197dfa
igorsieradzki <sieradzki.igor@gmail.com>
2015-06-17 21:44:10 +0200

    minor sorensen script change

--- experiments/fit_sorensen.py
+++ experiments/fit_sorensen.py
@@ -4,7 +4,7 @@ import numpy as np
 import sys
 from get_data import proteins
 
-def run(protein, batch_size, fingerprint="PubchemFP"):
+def run(protein, fingerprint="PubchemFP"):
 
     seed = 666
     warm_start_percentage = 0.05
@@ -18,42 +18,35 @@ def run(protein, batch_size, fingerprint="PubchemFP"):
 
     preprocess_fncs = [["to_binary", {"all_below": True}]]
 
+    for batch_size in [20, 50, 100]:
 
-    for c in list(np.linspace(0.1, 0.9, 9)):
-        svmtan_exp = run_experiment("fit_grid",
-                                force_reload=False,
-                                recalculate_experiments=False,
-                                n_jobs=5,
-                                experiment_detailed_name="fit_SVMTAN_czarnecki_two_clusters_sorensen_c_%.2f_%s_%s_%s" % (c, protein, fingerprint, str(batch_size)),
-                                base_experiment="fit_active_learning",
-                                seed=seed,
-                                grid_params={"strategy_kwargs:c": [c],
-                                             "strategy_kwargs:projection": ['sorensen']},
-                                base_experiment_kwargs={"strategy": "czarnecki_two_clusters",
-                                                        "preprocess_fncs": preprocess_fncs,
-                                                        "protein": protein,
-                                                        "fingerprint": fingerprint,
-                                                        "warm_start_percentage": warm_start_percentage,
-                                                        "batch_size": batch_size,
-                                                        "base_model": "SVMTAN",
-                                                        "loader_function": loader[0],
-                                                        "loader_args": loader[1],
-                                                        "param_grid": param_grid})
+        for c in list(np.linspace(0.1, 0.9, 9)):
+            svmtan_exp = run_experiment("fit_grid",
+                                    force_reload=False,
+                                    recalculate_experiments=False,
+                                    n_jobs=5,
+                                    experiment_detailed_name="fit_SVMTAN_czarnecki_two_clusters_sorensen_c_%.2f_%s_%s_%s" % (c, protein, fingerprint, str(batch_size)),
+                                    base_experiment="fit_active_learning",
+                                    seed=seed,
+                                    grid_params={"strategy_kwargs:c": [c],
+                                                 "strategy_kwargs:projection": ['sorensen']},
+                                    base_experiment_kwargs={"strategy": "czarnecki_two_clusters",
+                                                            "preprocess_fncs": preprocess_fncs,
+                                                            "protein": protein,
+                                                            "fingerprint": fingerprint,
+                                                            "warm_start_percentage": warm_start_percentage,
+                                                            "batch_size": batch_size,
+                                                            "base_model": "SVMTAN",
+                                                            "loader_function": loader[0],
+                                                            "loader_args": loader[1],
+                                                            "param_grid": param_grid})
 
 if __name__ == '__main__':
 
-    assert len(sys.argv) >= 3, "pass one protein and batch_size"
+    assert len(sys.argv) >= 1, "pass one protein and batch_size"
     protein = sys.argv[1]
-    batch_size = int(sys.argv[2])
-    fingerprint = "PubchemFP"
-    if len(sys.argv) == 4:
-        fingerprint = sys.argv[3]
     assert protein in proteins, "please pick one of proteins: %s" % proteins
-    assert batch_size in [20, 50, 100]
 
-    if fingerprint == "all":
-        run(protein, batch_size, "ExtFP")
-        run(protein, batch_size, "PubchemFP")
-        run(protein, batch_size, "MACCSFP")
-    else:
-        run(protein, batch_size, fingerprint)
+    run(protein, "ExtFP")
+    run(protein, "PubchemFP")
+    run(protein, "MACCSFP")
